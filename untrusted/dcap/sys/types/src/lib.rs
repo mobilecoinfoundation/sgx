92ff327d785e943ba87bcadfa87affd1010794c9
Nick Santana <nick@mobilecoin.com>
2022-05-25 10:40:30 -0700

    :q!
    fix clippy errors

--- untrusted/dcap/sys/types/src/lib.rs
+++ untrusted/dcap/sys/types/src/lib.rs
@@ -1,5 +1,5 @@
 // Copyright (c) 2022 The MobileCoin Foundation
 //
-#![allow(non_camel_case_types, non_snake_case, non_upper_case_globals)]
 
+#![allow(non_camel_case_types, non_snake_case, non_upper_case_globals, clippy::missing_safety_doc)]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
