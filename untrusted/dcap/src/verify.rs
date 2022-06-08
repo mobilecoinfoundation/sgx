d8902a48371423ef55a12b6b83b24bca62983fc3
Nick Santana <nick@mobilecoin.com>
2022-06-08 09:14:32 -0700

    tl;dr
    Remove unneeded imports and clean up the test

--- untrusted/dcap/src/verify.rs
+++ untrusted/dcap/src/verify.rs
@@ -12,11 +12,11 @@ pub trait Verify {
 
 impl Verify for Quote {
     fn verify(&self) -> Result<(), Error> {
-        // time_t -> pub type __time_t = ::std::os::raw::c_long;
-        let mut quote: Vec<u8> = vec![0; 8];
+        let quote = self.quote.as_ptr();
+        let quote_length = self.quote.len() as u32;
         let mut expiration_status = 1;
         let mut quote_verification_result = sgx_ql_qv_result_t::SGX_QL_QV_RESULT_UNSPECIFIED;
-        let result = unsafe { sgx_qv_verify_quote(quote.as_mut_ptr(), quote.len() as u32, ptr::null(),  0, &mut expiration_status, &mut quote_verification_result, ptr::null_mut(), 0, ptr::null_mut()) };
+        let result = unsafe { sgx_qv_verify_quote(quote, quote_length, ptr::null(),  1, &mut expiration_status, &mut quote_verification_result, ptr::null_mut(), 0, ptr::null_mut()) };
         match result {
             quote3_error_t::SGX_QL_SUCCESS => Ok(()),
             x => Err(Error::SgxStatus(x)),
@@ -27,18 +27,12 @@ impl Verify for Quote {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use mc_sgx_dcap_sys::{
-        quote3_error_t, sgx_qe_set_enclave_load_policy, sgx_ql_request_policy_t,
-    };
-    use mc_sgx_urts::{sgx_status_t, Enclave, EnclaveBuilder};
-    use std::mem::MaybeUninit;
-    use std::ptr;
-    use test_enclave::{ecall_create_report, ENCLAVE};
 
     #[test]
-    fn verify_quote() {
-        let quote = Quote{ quote: vec![0] };
+    fn verify_results_in_unsupported_format_when_empty_quote() {
+        // QUOTE_MIN_SIZE is 1020, so just round to a power of 2
+        let quote = Quote{ quote: vec![0; 1024] };
         let result = quote.verify();
-        assert!(result.is_err());
+        assert_eq!(result, Err(Error::SgxStatus(quote3_error_t::SGX_QL_QUOTE_FORMAT_UNSUPPORTED)));
     }
 }
