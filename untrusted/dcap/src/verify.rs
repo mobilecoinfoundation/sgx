7146b11cfadfca615255e94d9943e9d2df5bcbbf
Nick Santana <nick@mobilecoin.com>
2022-06-14 16:05:18 -0700

    .
    Adding an intermediate quote verification type

--- untrusted/dcap/src/verify.rs
+++ untrusted/dcap/src/verify.rs
@@ -7,6 +7,30 @@ use std::ptr;
 use std::time::{SystemTime, UNIX_EPOCH};
 use crate::{Error, Quote};
 
+/// The sgx_ql_qve_result_t has what are called terminal and non terminal values.
+/// They are described in
+/// https://download.01.org/intel-sgx/latest/dcap-latest/linux/docs/Intel_SGX_ECDSA_QuoteLibReference_DCAP_API.pdf,
+/// but not in the header.  We group them here to make it easier to re-use.
+enum QuoteVerificationResult {
+    Success,
+    Terminal(sgx_ql_qv_result_t),
+    NonTerminal(sgx_ql_qv_result_t),
+}
+
+impl From<sgx_ql_qv_result_t> for QuoteVerificationResult {
+    fn from(result: sgx_ql_qv_result_t) -> Self {
+        match result {
+            sgx_ql_qv_result_t::SGX_QL_QV_RESULT_OK => QuoteVerificationResult::Success,
+            sgx_ql_qv_result_t::SGX_QL_QV_RESULT_CONFIG_NEEDED |
+            sgx_ql_qv_result_t::SGX_QL_QV_RESULT_OUT_OF_DATE |
+            sgx_ql_qv_result_t::SGX_QL_QV_RESULT_OUT_OF_DATE_CONFIG_NEEDED |
+            sgx_ql_qv_result_t::SGX_QL_QV_RESULT_SW_HARDENING_NEEDED |
+            sgx_ql_qv_result_t::SGX_QL_QV_RESULT_CONFIG_AND_SW_HARDENING_NEEDED => QuoteVerificationResult::NonTerminal(result),
+            x => QuoteVerificationResult::Terminal(x),
+        }
+    }
+}
+
 pub trait Verify {
     fn verify(&self) -> Result<(), Error>;
 }
@@ -21,19 +45,15 @@ impl Verify for Quote {
         let result = unsafe { sgx_qv_verify_quote(quote, quote_length, ptr::null(), time, &mut expiration_status, &mut quote_verification_result, ptr::null_mut(), 0, ptr::null_mut()) };
         match result {
             quote3_error_t::SGX_QL_SUCCESS => {
-                match quote_verification_result {
-                    sgx_ql_qv_result_t::SGX_QL_QV_RESULT_OK => {
+                match quote_verification_result.into() {
+                    QuoteVerificationResult::Success => {
                         match expiration_status {
                             0 => Ok(()),
                             _ => Err(Error::CollateralExpired),
                         }
                     }
-                    x @ (sgx_ql_qv_result_t::SGX_QL_QV_RESULT_CONFIG_NEEDED |
-                    sgx_ql_qv_result_t::SGX_QL_QV_RESULT_OUT_OF_DATE |
-                    sgx_ql_qv_result_t::SGX_QL_QV_RESULT_OUT_OF_DATE_CONFIG_NEEDED |
-                    sgx_ql_qv_result_t::SGX_QL_QV_RESULT_SW_HARDENING_NEEDED |
-                    sgx_ql_qv_result_t::SGX_QL_QV_RESULT_CONFIG_AND_SW_HARDENING_NEEDED) => Err(Error::NonTerminal(quote3_error_t(x.0))),
-                    x => Err(Error::SgxStatus(quote3_error_t(x.0))),
+                    QuoteVerificationResult::NonTerminal(x) => Err(Error::NonTerminal(quote3_error_t(x.0))),
+                    QuoteVerificationResult::Terminal(x) => Err(Error::SgxStatus(quote3_error_t(x.0))),
                 }
             },
             x => Err(Error::SgxStatus(x)),
