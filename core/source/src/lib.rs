1292f1aa1f042c5950eef1c557ee2b0ee3146fdb
Nick Santana <nick@mobilecoin.com>
2022-07-28 15:23:53 -0700

    This will definitely break in 2044 (TODO)
    Adding in logic to do the build...??? maybe

--- core/source/src/lib.rs
+++ core/source/src/lib.rs
@@ -2,13 +2,31 @@
 //! Builds the linux SGX SDK
 
 use std::path::PathBuf;
+use std::process::Command;
+
+const SOURCE_PATH: &str = "../vendored/linux-sgx";
 
 struct Build {
 }
 
 impl Build {
     pub fn build() {
-        ../
+        let command = Command::new("make").current_dir(SOURCE_PATH).arg("preparation");
+        Self::run_command(command);
+        let command = Command::new("make").current_dir(SOURCE_PATH).arg("sdk_install_pkg");
+        Self::run_command(command);
+    }
 
+    fn run_command(command: &mut Command) {
+        let status = command.status();
+        match status {
+            Ok(status) => {
+                if status.success() {
+                    return
+                }
+                panic!("Exit code {}", status);
+            }
+            Err(fail) => panic!("Failed with: {}", failed),
+        }
     }
 }
