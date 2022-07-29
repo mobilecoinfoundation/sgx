559bdecbb7a6030c8ae325f6cb21a27a948d7745
Nick Santana <nick@mobilecoin.com>
2022-07-29 09:37:06 -0700

    Hide those navs, boi!
    have the source copying, going to build

--- core/source/build.rs
+++ core/source/build.rs
@@ -5,43 +5,49 @@ use std::{env, fs};
 use std::path::{Path, PathBuf};
 use std::process::Command;
 use cargo_emit::warning;
+use fs_extra::dir::CopyOptions;
 
 const SOURCE_PATH: &str = "vendored/linux-sgx";
 
 pub struct Build {
 }
 
-fn source_dir() -> PathBuf {
+fn sdk_source_dir() -> PathBuf {
     Path::new(&env::var("CARGO_MANIFEST_DIR").unwrap()).join(SOURCE_PATH)
 }
 
-fn build_dir() -> PathBuf {
-    Path::new(&env::var("OUT_DIR").unwrap()).join("linux-sgx")
+fn out_dir() -> PathBuf {
+    Path::new(&env::var("OUT_DIR").unwrap()).into()
 }
 
 fn main(){
-    let build_dir = build_dir();
+    let out_dir = out_dir();
 
-    warning!("The source dir {:?} and the build dir {:?}", source_dir(), build_dir);
-    fs_extra::dir::copy(source_dir(), &build_dir, &Default::default()).unwrap();
-    build_sdk(&build_dir);
-    write_out_sgx_sdk_path(&build_dir);
+    copy_sdk_to_build_dir(&sdk_source_dir(), &out_dir);
+    build_sdk(&out_dir);
+    write_out_sgx_sdk_path(&out_dir);
 }
 
-fn build_sdk<P: AsRef<Path>>(build_dir: &P) {
+fn copy_sdk_to_build_dir<P: AsRef<Path>>(sdk_source_dir: &P, out_dir: &P) {
+    let mut copy_options = CopyOptions::new();
+    copy_options.overwrite = true;
+    fs_extra::dir::copy(sdk_source_dir, out_dir, &copy_options).unwrap();
+}
+
+fn build_sdk<P: AsRef<Path>>(sdk_source_dir: &P) {
     let mut command = Command::new("make");
-    command.current_dir(build_dir).arg("preparation");
+    command.current_dir(sdk_source_dir).arg("preparation");
     run_command(command);
     let mut command = Command::new("make");
     //TODO not fond of the unlimited "-j" here, but on my machine build
     // times went from 8min down to 2min.  The unlimited "-j" can really
     // bog down the machine
-    command.current_dir(build_dir).arg("sdk_install_pkg").arg("-j");
+    command.current_dir(sdk_source_dir).arg("sdk_install_pkg").arg("-j");
     run_command(command);
 }
 
-fn write_out_sgx_sdk_path<P: AsRef<Path>>(build_dir: &P) {
-    let sgx_sdk_path = build_dir.as_ref().join("linux/installer/common/sdk/output/package");
+fn write_out_sgx_sdk_path<P: AsRef<Path>>(sdk_build_dir: &P) {
+    let sgx_sdk_path = sdk_build_dir.as_ref().join("linux/installer/common/sdk/output/package");
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     fs::write(out_dir.join("sgx_sdk_path.txt"), sgx_sdk_path.to_str().unwrap()).unwrap();
 }
