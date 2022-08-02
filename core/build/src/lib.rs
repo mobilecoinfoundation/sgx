51b547292c8aa5a511daf601fc4c9d3438b21995
Nick Santana <nick@mobilecoin.com>
2022-08-02 16:11:19 -0700

    Consolidate ParseCallbacks and Builder to common
    Adds an `SGXParseCallbacks` struct that can be used in a `Builder`.
    Adds a function, `sgx_builder()`  to provide a pre-configured `Builder`

--- core/build/src/lib.rs
+++ core/build/src/lib.rs
@@ -2,9 +2,56 @@
 
 #![doc = include_str!("../README.md")]
 
+use bindgen::{callbacks::ParseCallbacks, Builder, EnumVariation};
 use std::{env, path::PathBuf};
 
 static DEFAULT_SGX_SDK_PATH: &str = "/opt/intel/sgxsdk";
+
+/// Provides a default [bindgen::callbacks::ParserCallbacks::item_name]
+/// implementation that works with most SGX types.
+///
+/// # Arguments
+/// * `name` - The name of the type to determine the bindgen name of.
+pub fn sgx_item_name(name: &str) -> Option<String> {
+    if name.starts_with("_sgx") {
+        Some(name[1..].to_owned())
+    } else if name.starts_with('_') {
+        Some(format!("sgx{}", name))
+    } else {
+        None
+    }
+}
+
+/// Returns a builder configured with the defaults for using bindgen with the
+/// SGX libraries.
+pub fn sgx_builder() -> Builder {
+    Builder::default()
+        .derive_copy(true)
+        .derive_debug(true)
+        .derive_default(true)
+        .derive_eq(true)
+        .derive_hash(true)
+        .derive_ord(true)
+        .derive_partialeq(true)
+        .derive_partialord(true)
+        .default_enum_style(EnumVariation::Consts)
+        .prepend_enum_name(false)
+        .use_core()
+        .ctypes_prefix("core::ffi")
+        .allowlist_recursively(false)
+}
+
+/// SGXParseCallbacks to be used with [bindgen::Builder::parse_callbacks]
+///
+/// This provides a default implementation for most of the SGX libraries
+#[derive(Debug)]
+pub struct SGXParseCallbacks;
+
+impl ParseCallbacks for SGXParseCallbacks {
+    fn item_name(&self, name: &str) -> Option<String> {
+        sgx_item_name(name)
+    }
+}
 
 /// Return the SGX library path.
 ///
