42fe094897481a4e09f7ad4e65c991670d7960cc
James Cape <james@mobilecoin.com>
2022-08-12 15:40:15 -0700

    Fix bare URLs in quote_verify

--- quote_verify/src/lib.rs
+++ quote_verify/src/lib.rs
@@ -31,50 +31,50 @@ const ENCLAVE_REPORT_DATA_SIZE: usize = 64;
 
 // Size of the full ECDSA signature.
 // Table 6 of
-// https://download.01.org/intel-sgx/latest/dcap-latest/linux/docs/Intel_SGX_ECDSA_QuoteLibReference_DCAP_API.pdf
+// <https://download.01.org/intel-sgx/latest/dcap-latest/linux/docs/Intel_SGX_ECDSA_QuoteLibReference_DCAP_API.pdf>
 const SIGNATURE_SIZE: usize = 64;
 
 // Size of the full ECDSA key.
 // Table 7 of
-// https://download.01.org/intel-sgx/latest/dcap-latest/linux/docs/Intel_SGX_ECDSA_QuoteLibReference_DCAP_API.pdf
+// <https://download.01.org/intel-sgx/latest/dcap-latest/linux/docs/Intel_SGX_ECDSA_QuoteLibReference_DCAP_API.pdf>
 const KEY_SIZE: usize = 64;
 
 // The starting byte of the signature for the *ISV Enclave Report Signature* of
 // the Quote Signature Data Structure. Table 4 of
-// https://download.01.org/intel-sgx/latest/dcap-latest/linux/docs/Intel_SGX_ECDSA_QuoteLibReference_DCAP_API.pdf
+// <https://download.01.org/intel-sgx/latest/dcap-latest/linux/docs/Intel_SGX_ECDSA_QuoteLibReference_DCAP_API.pdf>
 // Note: the 4 is the for the *Quote Signature Data Len* from table 2.  The
 // variable length is _after_ the signature.
 const ISV_ENCLAVE_SIGNATURE_START: usize = QUOTE_HEADER_SIZE + ENCLAVE_REPORT_SIZE + 4;
 
 // The starting byte of the key for the *ECDSA Attestation Key* of
 // the Quote Signature Data Structure. Table 4 of
-// https://download.01.org/intel-sgx/latest/dcap-latest/linux/docs/Intel_SGX_ECDSA_QuoteLibReference_DCAP_API.pdf
+// <https://download.01.org/intel-sgx/latest/dcap-latest/linux/docs/Intel_SGX_ECDSA_QuoteLibReference_DCAP_API.pdf>
 const ATTESTATION_KEY_START: usize = ISV_ENCLAVE_SIGNATURE_START + SIGNATURE_SIZE;
 
 // The starting byte of the quote report.  The *QE Report* member of the Quote
 // Signature Data Structure.  Table 4 of
-// https://download.01.org/intel-sgx/latest/dcap-latest/linux/docs/Intel_SGX_ECDSA_QuoteLibReference_DCAP_API.pdf
+// <https://download.01.org/intel-sgx/latest/dcap-latest/linux/docs/Intel_SGX_ECDSA_QuoteLibReference_DCAP_API.pdf>
 const QUOTING_ENCLAVE_REPORT_START: usize = ATTESTATION_KEY_START + KEY_SIZE;
 
 // The starting byte of the signature for the quote. *QE Report Signature* of
 // the Quote Signature Data Structure. Table 4 of
-// https://download.01.org/intel-sgx/latest/dcap-latest/linux/docs/Intel_SGX_ECDSA_QuoteLibReference_DCAP_API.pdf
+// <https://download.01.org/intel-sgx/latest/dcap-latest/linux/docs/Intel_SGX_ECDSA_QuoteLibReference_DCAP_API.pdf>
 const QUOTING_ENCLAVE_SIGNATURE_START: usize = QUOTING_ENCLAVE_REPORT_START + ENCLAVE_REPORT_SIZE;
 
 // The starting byte of the report data from the quote report. *Report Data* of
 // the Enclave Report Body. Table 5 of
-// https://download.01.org/intel-sgx/latest/dcap-latest/linux/docs/Intel_SGX_ECDSA_QuoteLibReference_DCAP_API.pdf
+// <https://download.01.org/intel-sgx/latest/dcap-latest/linux/docs/Intel_SGX_ECDSA_QuoteLibReference_DCAP_API.pdf>
 const QUOTING_ENCLAVE_REPORT_DATA_START: usize = QUOTING_ENCLAVE_REPORT_START + 320;
 
 // The size of the message digest in the quoting enclave report data.  See
 // description of *QE Report* in table 4 of
-// https://download.01.org/intel-sgx/latest/dcap-latest/linux/docs/Intel_SGX_ECDSA_QuoteLibReference_DCAP_API.pdf
+// <https://download.01.org/intel-sgx/latest/dcap-latest/linux/docs/Intel_SGX_ECDSA_QuoteLibReference_DCAP_API.pdf>
 const QUOTING_ENCLAVE_REPORT_DATA_DIGEST_SIZE: usize = 32;
 
 // The starting byte of the quoting enclave authentication data size for the
 // quote.
 // *Size* from Table 8 of
-// https://download.01.org/intel-sgx/latest/dcap-latest/linux/docs/Intel_SGX_ECDSA_QuoteLibReference_DCAP_API.pdf
+// <https://download.01.org/intel-sgx/latest/dcap-latest/linux/docs/Intel_SGX_ECDSA_QuoteLibReference_DCAP_API.pdf>
 // which comes from the *QE Authentication Data* of the Quote Signature Data
 // Structure in Table 4.
 const QUOTING_ENCLAVE_AUTHENTICATION_DATA_SIZE_START: usize =
@@ -82,7 +82,7 @@ const QUOTING_ENCLAVE_AUTHENTICATION_DATA_SIZE_START: usize =
 
 // The starting byte of the quoting enclave authentication data for the quote.
 // *Data* from Table 8 of
-// https://download.01.org/intel-sgx/latest/dcap-latest/linux/docs/Intel_SGX_ECDSA_QuoteLibReference_DCAP_API.pdf
+// <https://download.01.org/intel-sgx/latest/dcap-latest/linux/docs/Intel_SGX_ECDSA_QuoteLibReference_DCAP_API.pdf>
 // which comes from the *QE Authentication Data* of the Quote Signature Data
 // Structure in Table 4.
 const QUOTING_ENCLAVE_AUTHENTICATION_DATA_START: usize =
@@ -92,7 +92,7 @@ const QUOTING_ENCLAVE_AUTHENTICATION_DATA_START: usize =
 //  offset, To be fixed with #25
 // The starting byte of the certification data for the quote.
 // *Certification Data* from Table 9 of
-// https://download.01.org/intel-sgx/latest/dcap-latest/linux/docs/Intel_SGX_ECDSA_QuoteLibReference_DCAP_API.pdf
+// <https://download.01.org/intel-sgx/latest/dcap-latest/linux/docs/Intel_SGX_ECDSA_QuoteLibReference_DCAP_API.pdf>
 // which comes from the *QE Certification Data* of the Quote Signature Data
 // Structure in Table 4.
 const QUOTING_ENCLAVE_CERTIFICATION_DATA_START: usize = 0x41C;
@@ -107,7 +107,7 @@ impl Quote {
     ///
     /// # Arguments
     ///
-    /// * `bytes` the bytes of the quote as defined in https://download.01.org/intel-sgx/latest/dcap-latest/linux/docs/Intel_SGX_ECDSA_QuoteLibReference_DCAP_API.pdf
+    /// * `bytes` the bytes of the quote as defined in <https://download.01.org/intel-sgx/latest/dcap-latest/linux/docs/Intel_SGX_ECDSA_QuoteLibReference_DCAP_API.pdf>
     pub fn from_bytes(bytes: &[u8]) -> Self {
         Quote {
             bytes: bytes.to_vec(),
@@ -129,7 +129,7 @@ impl Quote {
             .collect::<Result<Vec<_>, _>>()?;
 
         // Per Table 9 *Certification Data* (type 5) from
-        // https://download.01.org/intel-sgx/latest/dcap-latest/linux/docs/Intel_SGX_ECDSA_QuoteLibReference_DCAP_API.pdf
+        // <https://download.01.org/intel-sgx/latest/dcap-latest/linux/docs/Intel_SGX_ECDSA_QuoteLibReference_DCAP_API.pdf>
         // certs are in order from leaf -> .. -> root.  We need to start
         // verifying from the root.
         certs.reverse();
