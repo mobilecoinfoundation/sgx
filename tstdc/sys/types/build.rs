3ac6cb5e4a25f29c086794cfc3f04e6f3b7b4dec
James Cape <james@mobilecoin.com>
2022-08-12 17:04:11 -0700

    Fix tstdc attr stuff.

--- tstdc/sys/types/build.rs
+++ tstdc/sys/types/build.rs
@@ -2,12 +2,31 @@
 //! Builds the FFI type bindings for tstdc (trusted standard C) of the
 //! Intel SGX SDK
 
+use bindgen::callbacks::ParseCallbacks;
+
+#[derive(Debug)]
+struct TstdcParseCallbacks;
+
+impl ParseCallbacks for TstdcParseCallbacks {
+    fn item_name(&self, name: &str) -> Option<String> {
+        mc_sgx_core_build::normalize_item_name(name).map(|normie| match normie.as_str() {
+            "sgx_thread_cond_attr_t" | "sgx_thread_rwlock_attr_t" | "sgx_thread_mutex_attr_t" => {
+                normie.replace("_attr_", "attr_")
+            }
+            _ => normie,
+        })
+    }
+}
+
 fn main() {
     let include_path = mc_sgx_core_build::sgx_include_path();
+    cargo_emit::rerun_if_changed!(include_path);
 
     let out_path = mc_sgx_core_build::build_output_path();
 
     mc_sgx_core_build::sgx_builder()
+        // override the default ParseCallbacks impl provided by mc_sgx_core_build
+        .parse_callbacks(Box::new(TstdcParseCallbacks))
         .header("wrapper.h")
         .clang_arg(&format!("-I{}", include_path))
         .allowlist_recursively(false)
--- tstdc/sys/types/src/lib.rs
+++ tstdc/sys/types/src/lib.rs
@@ -5,4 +5,5 @@
 #![allow(non_camel_case_types, non_snake_case, non_upper_case_globals)]
 
 pub use core::ffi::c_size_t as size_t;
+
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
