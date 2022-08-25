8fb8e682daede5e2594af276476593d4025aec6d
James Cape <james@mobilecoin.com>
2022-08-25 14:50:23 -0700

    Create a new util crate, add some error helpers

---
+++ util/README.md
@@ -0,0 +1,21 @@
+# MobileCoin SGX: Utilities
+
+[![Project Chat][chat-image]][chat-link]<!--
+-->![License][license-image]<!--
+-->![Target][target-image]<!--
+-->[![Crates.io][crate-image]][crate-link]<!--
+-->[![Docs Status][docs-image]][docs-link]<!--
+-->[![Dependency Status][deps-image]][deps-link]
+
+Small utilities and helpers that are useful to have in a central place.
+
+[chat-image]: https://img.shields.io/discord/844353360348971068?style=flat-square
+[chat-link]: https://mobilecoin.chat
+[license-image]: https://img.shields.io/crates/l/mc-sgx-util?style=flat-square
+[target-image]: https://img.shields.io/badge/target-any-brightgreen?style=flat-square
+[crate-image]: https://img.shields.io/crates/v/mc-sgx-util.svg?style=flat-square
+[crate-link]: https://crates.io/crates/mc-sgx-util
+[docs-image]: https://img.shields.io/docsrs/mc-sgx-util?style=flat-square
+[docs-link]: https://docs.rs/crate/mc-sgx-util
+[deps-image]: https://deps.rs/crate/mc-sgx-util/0.1.0/status.svg?style=flat-square
+[deps-link]: https://deps.rs/crate/mc-sgx-util/0.1.0
---
+++ util/src/lib.rs
@@ -0,0 +1,86 @@
+// Copyright (c) 2022 The MobileCoin Foundation
+
+#![doc = include_str!("../README.md")]
+#![no_std]
+#![deny(missing_docs, missing_debug_implementations, unsafe_code)]
+
+/// A trait to add to an error type which can be constructed from an underlying
+/// "status" type which contains both success and failure codes.
+///
+/// The goal is to be able to convert a unified status type into a `Result<T,
+/// Error>` type.
+///
+/// # Examples
+///
+/// ```rust
+/// use mc_sgx_util::ResultFrom;
+///
+/// /// An example FFI type
+/// #[allow(non_camel_case_types)]
+/// pub struct a_status_t(u32);
+///
+/// impl a_status_t {
+///     const SUCCESS: a_status_t = a_status_t(0);
+///     const FAIL: a_status_t = a_status_t(1);
+/// }
+///
+/// /// An example rusty enum wrapper for a_status_t
+/// pub enum AnError {
+///     Stuff,
+///     Unknown
+/// }
+///
+/// impl TryFrom<a_status_t> for AnError {
+///     type Error = ();
+///
+///     fn try_from(value: a_status_t) -> Result<Self, ()> {
+///         match value {
+///             a_status_t::SUCCESS => Err(()),
+///             a_status_t::FAIL => Ok(AnError::Stuff),
+///             _ => Ok(AnError::Unknown)
+///         }
+///     }
+/// }
+///
+/// // Pick up the default implementation of [`ResultFrom`]
+/// impl ResultFrom<a_status_t> for AnError {}
+///
+/// let status = a_status_t::SUCCESS;
+/// assert_eq!(Ok(()), AnError::result_from(status));
+///
+/// let status = a_status_t::FAIL;
+/// assert_eq!(Err(AnError::Stuff), AnError::result_from(status));
+/// ```
+pub trait ResultFrom<ST>: TryFrom<ST> {
+    /// Flips the result of a `TryFrom`.
+    fn result_from(status: ST) -> Result<<Self as TryFrom<ST>>::Error, Self> {
+        match Self::try_from(status) {
+            Ok(err) => Err(err),
+            Err(success_val) => Ok(success_val),
+        }
+    }
+}
+
+/// An inverse of the [`ResultFrom`] trait, which is attached to the status
+/// type.
+///
+/// As with [`TryInto`](core::convert::TryInto), this trait is not intended to
+/// be implemented manually, but instead should be added to an FFI type via
+/// blanket implementation attached to it's derivative wrapper type.
+///
+/// That is, users should not attach this to a bindgen-generated status type,
+/// they should attach [`ResultFrom`] to the intended error wrapper associated
+/// with it, and they will get this for free.
+pub trait ResultInto<T: TryFrom<Self>>: Sized {
+    /// Flips the result of T::TryFrom<Self>.
+    fn into_result(self) -> Result<<T as TryFrom<Self>>::Error, T> {
+        match T::try_from(self) {
+            Ok(err) => Err(err),
+            Err(success_val) => Ok(success_val),
+        }
+    }
+}
+
+/// This blanket implementation exists to automatically add this to any FFI type
+/// which has an inverse [`ResultFrom`] implementation.
+impl<T, ST> ResultInto<T> for ST where T: ResultFrom<ST> {}
