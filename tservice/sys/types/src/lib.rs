016cce6d41c21984433571320913804d9c4a90ed
Nick Santana <nick@mobilecoin.com>
2022-09-01 09:40:13 -0700

    Add tservice types crate

--- tservice/sys/types/src/lib.rs
+++ tservice/sys/types/src/lib.rs
@@ -12,8 +12,12 @@
 
 use core::ffi::c_size_t as size_t;
 use mc_sgx_core_sys_types::{
-    sgx_attributes_t, sgx_cpu_svn_t, sgx_isv_svn_t, sgx_key_request_t, sgx_measurement_t,
-    sgx_misc_select_t, sgx_prod_id_t, sgx_report_t, sgx_target_info_t,
+    default_for_byte_struct, sgx_attributes_t, sgx_cpu_svn_t, sgx_isv_svn_t, sgx_key_request_t,
+    sgx_measurement_t, sgx_misc_select_t, sgx_prod_id_t, sgx_report_t, sgx_target_info_t,
 };
 use mc_sgx_tcrypto_sys_types::sgx_ec256_public_t;
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+default_for_byte_struct! {
+    sgx_dh_session_t, sgx_dh_session, SGX_DH_SESSION_DATA_SIZE;
+}
---
+++ tservice/types/src/lib.rs
@@ -0,0 +1,8 @@
+// Copyright (c) 2022 The MobileCoin Foundation
+
+#![doc = include_str!("../README.md")]
+#![no_std]
+
+mod diffie_hellman;
+
+pub use diffie_hellman::Session;
