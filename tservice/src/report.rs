92d97b278a741319ca00bf60b9d3525825efb4e6
Nick Santana <nick@mobilecoin.com>
2022-09-14 08:51:55 -0700

    Rework `mc-sgx-tservice::Report` trait
    Previously the `mc-sgx-tservice::Report` trait had method
    implementations was explicit to `mc-sgx-core-types::Report` and was
    lacking an impl block. Now the `mc-sgx-tservice::Report` trait is
    method signatures and there is an impl block for of the
    `mc-sgx-tservice::Report` for the `mc-sgx-core-types::Report` type.
    
    This also renamed the `mc-sgx-tservice::Report` trait methods:
    - `create()` -> `new()`
    - `verify_report()` -> `verify()`

--- tservice/src/report.rs
+++ tservice/src/report.rs
@@ -7,17 +7,24 @@ use mc_sgx_core_types::{ReportData, Result, TargetInfo};
 use mc_sgx_util::ResultInto;
 
 /// Report operations that can be performed inside of an enclave
-pub trait Report {
+pub trait Report: Sized {
     /// Creates a report for the current enclave.
     ///
     /// # Arguments
     /// * `target_info` - The information for the target enclave which will
-    ///   cryptographically verify the report.  See [`Report::verify_report()`].
+    ///   cryptographically verify the report.  See [`Report::verify()`].
     /// * `report_data` - Report data used to communicate between enclaves
-    fn create_report(
-        target_info: &TargetInfo,
-        report_data: Option<&ReportData>,
-    ) -> Result<mc_sgx_core_types::Report> {
+    fn new(target_info: &TargetInfo, report_data: Option<&ReportData>) -> Result<Self>;
+
+    /// Verify an enclave report
+    ///
+    /// Ok means that the report was generated using a [`TargetInfo`] that
+    /// matches the one for the enclave making this call.
+    fn verify(&self) -> Result<()>;
+}
+
+impl Report for mc_sgx_core_types::Report {
+    fn new(target_info: &TargetInfo, report_data: Option<&ReportData>) -> Result<Self> {
         let mut report = sgx_report_t::default();
         unsafe {
             mc_sgx_tservice_sys::sgx_create_report(
@@ -30,15 +37,8 @@ pub trait Report {
         Ok(report.into())
     }
 
-    /// Verify an enclave report
-    ///
-    /// Ok means that the input report was generated using a [`TargetInfo`]
-    /// that matches the one for the enclave making this call.
-    ///
-    /// # Arguments
-    /// * `report` - The report to verify
-    fn verify_report(report: &mc_sgx_core_types::Report) -> Result<()> {
-        unsafe { mc_sgx_tservice_sys::sgx_verify_report(report.as_ref()) }.into_result()?;
+    fn verify(&self) -> Result<()> {
+        unsafe { mc_sgx_tservice_sys::sgx_verify_report(self.as_ref()) }.into_result()?;
         Ok(())
     }
 }
