8c721923056e89511f187e9c79e1228e5b581134
Andrew Wygle <andrew@mobilecoin.com>
2022-09-14 17:16:00 -0700

    Make KssConfig a newtype wrapper

--- urts/sys/types/src/lib.rs
+++ urts/sys/types/src/lib.rs
@@ -7,3 +7,12 @@ use mc_sgx_core_sys_types::{
     sgx_attributes_t, sgx_config_id_t, sgx_config_svn_t, sgx_misc_select_t,
 };
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+impl Default for sgx_kss_config_t {
+    fn default() -> sgx_kss_config_t {
+        sgx_kss_config_t {
+            config_id: [0; 64],
+            config_svn: 0,
+        }
+    }
+}
