a9fd571960dd5751d29deb255783dcbbb41bbf8f
awygle <awygle@gmail.com>
2022-09-15 14:19:13 -0700

    Use SGX_CONFIGID_SIZE constant for size of config_id array
    Co-authored-by: Nick Santana <nick@mobilecoin.com>

--- urts/sys/types/src/lib.rs
+++ urts/sys/types/src/lib.rs
@@ -4,14 +4,14 @@
 #![allow(non_camel_case_types, non_snake_case, non_upper_case_globals)]
 
 use mc_sgx_core_sys_types::{
-    sgx_attributes_t, sgx_config_id_t, sgx_config_svn_t, sgx_misc_select_t,
+    sgx_attributes_t, sgx_config_id_t, sgx_config_svn_t, sgx_misc_select_t, SGX_CONFIGID_SIZE,
 };
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 impl Default for sgx_kss_config_t {
     fn default() -> sgx_kss_config_t {
         sgx_kss_config_t {
-            config_id: [0; 64],
+            config_id: [0; SGX_CONFIGID_SIZE],
             config_svn: 0,
         }
     }
