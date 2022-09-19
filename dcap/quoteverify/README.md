a503951c45372ca937f50d72499526000c25be5b
Nick Santana <nick@mobilecoin.com>
2022-09-19 13:34:15 -0700

    Add `mc-sgx-dcap_quoteverify` crate

---
+++ dcap/quoteverify/README.md
@@ -0,0 +1,48 @@
+# MobileCoin SGX: DCAP Quote Verification
+
+[![Project Chat][chat-image]][chat-link]<!--
+-->![License][license-image]<!--
+-->![Target][target-image]<!--
+-->[![Crates.io][crate-image]][crate-link]<!--
+-->[![Docs Status][docs-image]][docs-link]<!--
+-->[![Dependency Status][deps-image]][deps-link]
+
+Idiomatic rust wrappers for the `sgx_dcap_quoteverify` library.
+
+This crate provides rust wrappers to support verifying quotes for Intel SGX DCAP
+attestation.
+
+If you're looking for using the Intel&reg; DCAP libraries you may be interested
+in:
+
+- [DCAP Orientation Guide][dcap-orientation-guide]
+- [DCAP Installation][dcap-installation]
+- [DCAP Reference][dcap-reference]
+- [DCAP Repo][dcap-repo]
+
+## Minimum Supported Rust Version
+
+Rust **1.62** or higher.
+
+Minimum supported Rust version can be changed in the future, but it will be done with a minor version bump.
+
+## SemVer Policy
+
+- All on-by-default features of this library are covered by SemVer
+- MSRV is considered exempt from SemVer as noted above
+
+[chat-image]: https://img.shields.io/discord/844353360348971068?style=flat-square
+[chat-link]: https://mobilecoin.chat
+[license-image]: https://img.shields.io/crates/l/mc-sgx-dcap-quoteverify?style=flat-square
+[target-image]: https://img.shields.io/badge/target-x86__64-blue?style=flat-square
+[crate-image]: https://img.shields.io/crates/v/mc-sgx-dcap-quoteverify.svg?style=flat-square
+[crate-link]: https://crates.io/crates/mc-sgx-dcap-quoteverify
+[docs-image]: https://img.shields.io/docsrs/mc-sgx-dcap-quoteverify?style=flat-square
+[docs-link]: https://docs.rs/crate/mc-sgx-dcap-quoteverify
+[deps-image]: https://deps.rs/crate/mc-sgx-dcap-quoteverify/0.1.0/status.svg?style=flat-square
+[deps-link]: https://deps.rs/crate/mc-sgx-dcap-quoteverify/0.1.0
+
+[dcap-reference]: <https://download.01.org/intel-sgx/latest/dcap-latest/linux/docs/Intel_SGX_ECDSA_QuoteLibReference_DCAP_API.pdf>
+[dcap-repo]: <https://github.com/intel/SGXDataCenterAttestationPrimitives>
+[dcap-installation]: <https://www.intel.com/content/www/us/en/developer/articles/guide/intel-software-guard-extensions-data-center-attestation-primitives-quick-install-guide.html>
+[dcap-orientation-guide]: <https://www.intel.com/content/www/us/en/developer/articles/guide/intel-software-guard-extensions-data-center-attestation-primitives-quick-install-guide.html>
---
+++ dcap/quoteverify/src/lib.rs
@@ -0,0 +1,97 @@
+// Copyright (c) 2022 MobileCoin Foundation
+
+#![doc = include_str!("../README.md")]
+#![deny(missing_docs, missing_debug_implementations)]
+
+use mc_sgx_dcap_quoteverify_types::Path as QvPath;
+use mc_sgx_dcap_types::Quote3Error;
+use mc_sgx_util::ResultInto;
+use std::{ffi::CString, os::unix::ffi::OsStrExt, path::Path};
+
+/// Set path for QVE(Quote verification enclave) or QPL(Quote provider library)
+///
+/// This allows one to override the path that will be searched for each
+/// `path_type`.  When this isn't called then the local path and dlopen search
+/// path will be utilized.
+///
+/// Returns [`Quote3Error`] when
+/// * `path` does not point to a file
+/// * `path` is longer than 259 (bytes)
+/// * `path` contains a null (0) byte.
+///
+/// # Arguments
+/// * `path_type` - Which path to set
+/// * `path` - The path value to use.  This is the full path to a file.
+pub fn set_path<P: AsRef<Path>>(path_type: QvPath, path: P) -> Result<(), Quote3Error> {
+    let c_path = CString::new(path.as_ref().as_os_str().as_bytes())
+        .map_err(|_| Quote3Error::InvalidParameter)?;
+    unsafe { mc_sgx_dcap_quoteverify_sys::sgx_qv_set_path(path_type.into(), c_path.as_ptr()) }
+        .into_result()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use mc_sgx_dcap_quoteverify_types::Path::{QuoteProviderLibrary, QuoteVerificationEnclave};
+    use std::fs;
+    use tempfile::tempdir;
+
+    #[test]
+    fn qve_path_succeeds() {
+        let dir = tempdir().unwrap();
+        let file_name = dir.path().join("fake.txt");
+        fs::write(&file_name, "stuff").unwrap();
+        assert!(set_path(QuoteVerificationEnclave, file_name).is_ok());
+    }
+
+    #[test]
+    fn qpl_path_succeeds() {
+        let dir = tempdir().unwrap();
+        let file_name = dir.path().join("fake.txt");
+        fs::write(&file_name, "stuff").unwrap();
+        assert!(set_path(QuoteProviderLibrary, file_name).is_ok());
+    }
+
+    #[test]
+    fn path_as_directory_fails() {
+        let dir = tempdir().unwrap();
+        assert!(set_path(QuoteVerificationEnclave, dir.path()).is_err());
+    }
+
+    #[test]
+    fn path_with_0_byte_fails_in_c_string() {
+        let dir = tempdir().unwrap();
+        let file_name = dir.path().join("fake\0.txt");
+        // fs::write() will fail to create the file with a null byte in the path
+        // so we pass the path as non existent to `set_path`.
+        assert!(set_path(QuoteVerificationEnclave, file_name).is_err());
+    }
+
+    #[test]
+    fn path_length_at_max_ok() {
+        const MAX_PATH: usize = 259;
+        let dir = tempdir().unwrap();
+        let mut dir_length = dir.path().as_os_str().as_bytes().len();
+        dir_length += 1; // for the joining "/"
+
+        let long_name = str::repeat("a", MAX_PATH - dir_length);
+        let file_name = dir.path().join(long_name);
+        fs::write(&file_name, "stuff").unwrap();
+
+        assert!(set_path(QuoteProviderLibrary, file_name).is_ok());
+    }
+
+    #[test]
+    fn path_length_exceeded() {
+        const MAX_PATH: usize = 259;
+        let dir = tempdir().unwrap();
+        let mut dir_length = dir.path().as_os_str().as_bytes().len();
+        dir_length += 1; // for the joining "/"
+
+        let long_name = str::repeat("a", (MAX_PATH + 1) - dir_length);
+        let file_name = dir.path().join(long_name);
+        fs::write(&file_name, "stuff").unwrap();
+
+        assert!(set_path(QuoteProviderLibrary, file_name).is_err());
+    }
+}
