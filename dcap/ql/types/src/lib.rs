8125738486c1153aa138f1e470f917709444f072
Nick Santana <nick@mobilecoin.com>
2022-09-27 08:26:24 -0700

    Rename `mc_sgx_dcap_ql_types::Path` to `PathKind`
    Also fixed typo for the `mc_sgx_dcap_ql_types` in the Cargo.toml

--- dcap/ql/types/src/lib.rs
+++ dcap/ql/types/src/lib.rs
@@ -10,7 +10,7 @@ use mc_sgx_dcap_ql_sys_types::sgx_ql_path_type_t;
 /// Paths (location and filename) to be override the default entries.
 #[non_exhaustive]
 #[derive(Eq, PartialEq, Debug)]
-pub enum Path {
+pub enum PathKind {
     /// Quoting Enclave (QE3)
     QuotingEnclave,
     /// Provisioning Certificate Enclave (PCE)
@@ -21,7 +21,7 @@ pub enum Path {
     IdEnclave,
 }
 
-impl TryFrom<sgx_ql_path_type_t> for Path {
+impl TryFrom<sgx_ql_path_type_t> for PathKind {
     type Error = FfiError;
 
     fn try_from(p: sgx_ql_path_type_t) -> Result<Self, Self::Error> {
@@ -35,13 +35,13 @@ impl TryFrom<sgx_ql_path_type_t> for Path {
     }
 }
 
-impl From<Path> for sgx_ql_path_type_t {
-    fn from(p: Path) -> sgx_ql_path_type_t {
+impl From<PathKind> for sgx_ql_path_type_t {
+    fn from(p: PathKind) -> sgx_ql_path_type_t {
         match p {
-            Path::QuotingEnclave => sgx_ql_path_type_t::SGX_QL_QE3_PATH,
-            Path::ProvisioningCertificateEnclave => sgx_ql_path_type_t::SGX_QL_PCE_PATH,
-            Path::QuoteProviderLibrary => sgx_ql_path_type_t::SGX_QL_QPL_PATH,
-            Path::IdEnclave => sgx_ql_path_type_t::SGX_QL_IDE_PATH,
+            PathKind::QuotingEnclave => sgx_ql_path_type_t::SGX_QL_QE3_PATH,
+            PathKind::ProvisioningCertificateEnclave => sgx_ql_path_type_t::SGX_QL_PCE_PATH,
+            PathKind::QuoteProviderLibrary => sgx_ql_path_type_t::SGX_QL_QPL_PATH,
+            PathKind::IdEnclave => sgx_ql_path_type_t::SGX_QL_IDE_PATH,
         }
     }
 }
@@ -53,29 +53,29 @@ mod test {
     use super::*;
 
     #[parameterized(
-    qe3 = { sgx_ql_path_type_t::SGX_QL_QE3_PATH, Path::QuotingEnclave },
-    pce = { sgx_ql_path_type_t::SGX_QL_PCE_PATH, Path::ProvisioningCertificateEnclave },
-    qpl = { sgx_ql_path_type_t::SGX_QL_QPL_PATH, Path::QuoteProviderLibrary },
-    ide = { sgx_ql_path_type_t::SGX_QL_IDE_PATH, Path::IdEnclave },
+    qe3 = { sgx_ql_path_type_t::SGX_QL_QE3_PATH, PathKind::QuotingEnclave },
+    pce = { sgx_ql_path_type_t::SGX_QL_PCE_PATH, PathKind::ProvisioningCertificateEnclave },
+    qpl = { sgx_ql_path_type_t::SGX_QL_QPL_PATH, PathKind::QuoteProviderLibrary },
+    ide = { sgx_ql_path_type_t::SGX_QL_IDE_PATH, PathKind::IdEnclave },
     )]
-    fn from_sgx_to_path(sgx_path: sgx_ql_path_type_t, expected: Path) {
-        let path: Path = sgx_path.try_into().unwrap();
+    fn from_sgx_to_path(sgx_path: sgx_ql_path_type_t, expected: PathKind) {
+        let path: PathKind = sgx_path.try_into().unwrap();
         assert_eq!(path, expected);
     }
 
     #[parameterized(
-    qe3 = { Path::QuotingEnclave, sgx_ql_path_type_t::SGX_QL_QE3_PATH },
-    pce = { Path::ProvisioningCertificateEnclave, sgx_ql_path_type_t::SGX_QL_PCE_PATH },
-    qpl = { Path::QuoteProviderLibrary, sgx_ql_path_type_t::SGX_QL_QPL_PATH },
-    ide = { Path::IdEnclave, sgx_ql_path_type_t::SGX_QL_IDE_PATH },
+    qe3 = { PathKind::QuotingEnclave, sgx_ql_path_type_t::SGX_QL_QE3_PATH },
+    pce = { PathKind::ProvisioningCertificateEnclave, sgx_ql_path_type_t::SGX_QL_PCE_PATH },
+    qpl = { PathKind::QuoteProviderLibrary, sgx_ql_path_type_t::SGX_QL_QPL_PATH },
+    ide = { PathKind::IdEnclave, sgx_ql_path_type_t::SGX_QL_IDE_PATH },
     )]
-    fn from_path_to_sgx(path: Path, expected: sgx_ql_path_type_t) {
+    fn from_path_to_sgx(path: PathKind, expected: sgx_ql_path_type_t) {
         let sgx_path: sgx_ql_path_type_t = path.into();
         assert_eq!(sgx_path, expected);
     }
     #[test]
     fn sgx_path_out_of_bounds_panics() {
-        let result = Path::try_from(sgx_ql_path_type_t(4));
+        let result = PathKind::try_from(sgx_ql_path_type_t(4));
         assert!(result.is_err());
     }
 }
