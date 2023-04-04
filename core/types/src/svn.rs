f70d0f419dbc6bd3d6277107c3f70b35d5429fe2
Sam <sam.dealy@gmail.com>
2023-04-04 15:44:00 -0700

    Implement Display for IsvSvn

--- core/types/src/svn.rs
+++ core/types/src/svn.rs
@@ -1,7 +1,7 @@
 // Copyright (c) 2022-2023 The MobileCoin Foundation
 //! SGX core SVN (Security Version Numbers)
 
-use crate::{impl_newtype, impl_newtype_for_bytestruct};
+use crate::{impl_newtype, impl_newtype_for_bytestruct, impl_newtype_no_display};
 use mc_sgx_core_sys_types::{sgx_config_svn_t, sgx_cpu_svn_t, sgx_isv_svn_t, SGX_CPUSVN_SIZE};
 
 /// Config security version number (SVN)
@@ -18,7 +18,7 @@ impl_newtype! {
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq, Default)]
 pub struct IsvSvn(sgx_isv_svn_t);
 
-impl_newtype! {
+impl_newtype_no_display! {
     IsvSvn, sgx_isv_svn_t;
 }
 
@@ -44,6 +44,17 @@ mod test {
 
         let display_string = format!("{}", cpu_svn);
         let expected_string = "0x0101_0101_0101_0101_0101_0101_0101_0101";
+
+        assert_eq!(display_string, expected_string);
+    }
+
+    #[test]
+    fn isv_svn_display() {
+        let sgx_isv_svn_t = 3459;
+        let isv_svn = IsvSvn::from(sgx_isv_svn_t);
+
+        let display_string = format!("{}", isv_svn);
+        let expected_string = "0x0D83";
 
         assert_eq!(display_string, expected_string);
     }
