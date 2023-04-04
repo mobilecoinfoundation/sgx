fed0918724b2a4a15b99365a3fafdc91f7eb9480
Sam <sam.dealy@gmail.com>
2023-04-04 15:30:49 -0700

    Implement Display for ConfigId

--- core/types/src/config_id.rs
+++ core/types/src/config_id.rs
@@ -1,7 +1,8 @@
 // Copyright (c) 2022-2023 The MobileCoin Foundation
 //! SGX Config ID
 
-use crate::impl_newtype;
+use crate::impl_newtype_no_display;
+use core::fmt::{Display, Formatter};
 use mc_sgx_core_sys_types::{sgx_config_id_t, SGX_CONFIGID_SIZE};
 
 /// Config ID
@@ -9,13 +10,38 @@ use mc_sgx_core_sys_types::{sgx_config_id_t, SGX_CONFIGID_SIZE};
 #[repr(transparent)]
 pub struct ConfigId(sgx_config_id_t);
 
-impl_newtype! {
+impl_newtype_no_display! {
     ConfigId, sgx_config_id_t;
+}
+
+impl Display for ConfigId {
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
+        write!(f, "ConfigId: ")?;
+        mc_sgx_util::fmt_hex(&self.0, f)
+    }
 }
 
 // Pure array type larger than 32 so must implement default at the newtype level
 impl Default for ConfigId {
     fn default() -> Self {
         Self::from([0; SGX_CONFIGID_SIZE])
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate std;
+    use crate::ConfigId;
+    use std::format;
+
+    #[test]
+    fn display_config_id() {
+        let sgx_config_id_t = [34u8; 64];
+        let config_id = ConfigId::from(sgx_config_id_t);
+
+        let display_string = format!("{}", config_id);
+        let expected = "ConfigId: 2222_2222_2222_2222_2222_2222_2222_2222_2222_2222_2222_2222_2222_2222_2222_2222_2222_2222_2222_2222_2222_2222_2222_2222_2222_2222_2222_2222_2222_2222_2222_2222";
+
+        assert_eq!(display_string, expected);
     }
 }
